0b352547ea923a0b8b5dc1ad1a7e99767b2da4f1
VarvaraSorokina <99732956+VarvaraSorokina@users.noreply.github.com>
2022-12-22 13:59:01 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,68 @@
+//Пример №1
+let a1 = 7 + 2, a2 = 7 - 3, a3 = 2 * 4, a4 = 4 / 2;
+console.log(a1, a2, a3, a4);
+
+
+//Пример №2
+let height = 23, width = 10;
+let s = height * width;
+console.log(s);
+
+//Пример №3
+let b1 = 5 % 3, b2 = 3 % 5, b3 = 5 + '3', b4 = '5' - 3, b5 = 75 +'кг';
+console.log(b1, b2, b3, b4, b5);
+
+//Пример №4
+let heightC = 10, dC = 4, v;
+v = 3,14 * ((dC ** 2) / 4) *heightC;
+console.log(v);
+
+//Пример №5
+let a_5 = 10, b_5 = 2;
+console.log(a_5+b_5, a_5-b_5, a_5*b_5, a_5/b_5);
+
+//Пример №6
+let c_6 = 15, d_6 = 2, result_6= c_6+d_6;
+console.log(result_6);
+
+//Пример №7
+let a_7 = 10, b_7 = 2, c_7 = 5;
+console.log(a_7+b_7+c_7);
+
+//Пример №8
+let a_8 = 17, b_8 = 10, c_8 = a_8-b_8, d_8 = 7, result_8 = c_8+d_8;
+console.log(result_8);
+
+//Пример №9
+let name = 'Varvara';
+console.log(`Привет, ${name}`);
+
+//Пример №10
+let age = 20;
+console.log(`Мне ${age} лет!`);
+
+// Пример №11
+let str = 'abcde';
+console.log(str[0], str[2], str[4]);
+
+// Пример №12
+let hour = 60, day = 24, month = 30;
+let sec_hour = hour * 60;
+let sec_day = sec_hour * day;
+let sec_month = sec_day * month;
+console.log(sec_hour, sec_day, sec_month);
+
+//Пример №13
+let num_13 = 47;
+num_13 += 7;
+num_13 -= 18;
+num_13 *= 10;
+num_13 /= 15;
+console.log(num_13);
+
+//Пример №14
+let num_14 = 10;
+num_14 ++;
+num_14 ++;
+num_14 --;
+console.log(num_14);
